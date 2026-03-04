195a82ff957ae8b8342f8c02fc09ccb0ef565a7e
Ramy945 <ramymostafa945@gmail.com>
2026-03-04 14:38:58 +0100

    .

---
+++ src/test/java/com/example/studybuddybackend/StudybuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studybuddybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybuddyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
